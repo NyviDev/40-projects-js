84841f9ba4272d3a2adc844d914ba417b5fbb228
Nyvi Vieira <nicollyvscosta@gmail.com>
2022-08-11 10:02:14 -0300

    project9 done

---
+++ project9/main.js
@@ -0,0 +1,13 @@
+const btn = document.querySelector('.switch-btn')
+const video = document.querySelector('.video-container')
+
+btn.addEventListener('click', () => {
+    if(!btn.classList.contains('slide')) {
+        btn.classList.add('slide')
+        video.pause()
+    } else {
+        btn.classList.remove('slide')
+        video.play()
+
+    }
+})
